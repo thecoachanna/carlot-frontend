8c6f0126ffa190b43968f65e79179324c534f596
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-07-26 12:54:55 -0700

    searchbar is completed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Routes, Route} from 'react-router-dom'
+import ShowCar from './pages/ShowCar'
 import NewCar from './pages/NewCar';
 import Home from './pages/Home';
-import ShowCar from './pages/ShowCar';
 
 
 
 function App() {
 
-  const [cars, setCars] = useState([])
+  const [ cars, setCars  ] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:4000/cars/')
-      .then(res => res.json())
-      .then(cars => setCars(cars))
+    .then( res => res.json())
+    .then( cars => setCars(cars))
   }, [])
   
   console.log(cars)
 
   const addCar = (car) => {
     setCars([...cars, car])
-  }
+ }
 
   return (
     <div className="App">
       <Routes>
-
-        <Route path='/' element={<Home cars={cars} setCars={setCars}/>} />
-        <Route path='/newCar' element={<NewCar addCar={addCar} />} />
-        <Route path='/cars/:id' element={<ShowCar cars={cars} />} />
+        <Route path='/' element = { <Home cars={cars} setCars={setCars} />} />
+        <Route path="/newCar" element={ <NewCar addCar={addCar} />} />
+        <Route path='/cars/:id' element={ <ShowCar car={cars} />} />
       </Routes>
+      
     </div>
   );
 }
+
 export default App;
