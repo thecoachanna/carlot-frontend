d7f82aaca03db6c1ac2d3903efd1cb8ab8eb5864
thecoachanna <nettles.anna@gmail.com>
2022-07-30 10:37:02 -0400

    updated changes

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import {useState} from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -17,7 +17,7 @@ const Review = ({addReview, reviews}) => {
         reviewer: ''
        }
 
-const navigate = useNavigate()
+// const navigate = useNavigate()
 
 const [formData, setFormData] = useState(initialState)
   
@@ -51,6 +51,6 @@ const handleSubmit = (e) => {
     </div>
   )
 }
-// }
+
 
 export default Review
