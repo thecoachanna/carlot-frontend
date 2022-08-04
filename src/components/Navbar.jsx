44d2cf8168f0519b0ff4df820433c917fd47694b
joeavery-ruiz <joeavery96@yahoo.com>
2022-08-03 19:33:08 -0500

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({user, setUser}) => {
     }
 
   return (
-    <NavbarContainer>
+   /*  <NavbarContainer>
         <ul>
             <li> <Link to='/'> Home </Link></li>
             <li> <Link to='/newcar'> New Listing</Link></li>
@@ -37,6 +37,51 @@ const Navbar = ({user, setUser}) => {
             <li><a href='#' onClick={logout}>Logout</a></li>
         </ul>
     </NavbarContainer>
+ */
+    <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+        <img src="https://png.clipart.me/image_preview/2eb/sport-car-27559.png" alt="" width="120" height="72" class="d-inline-block align-text-top"></img>
+    </a> 
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/newcar">New Listing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Signup">Signup</a>
+        </li>
+    
+        <li class="nav-item">
+            <a class="nav-link" href='#' onClick={logout}>Logout</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
   )
 }
 
