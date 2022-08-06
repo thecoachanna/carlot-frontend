232434ccb6a0b58da4e8e0504040340936da0dc9
sashaharmer@yahoo.com <sashaharmer@yahoo.com>
2022-08-05 20:59:12 -0400

    commiting before deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { setToken } from '../utils/tokenServices'
 import { getUserFromPayload } from '../utils/tokenServices'
 import SearchBar from './searchBar/SearchBar'
